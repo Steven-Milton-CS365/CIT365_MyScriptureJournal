a9ba09b4477246397133b66778dbfb6e3bb71efa
Steven D Milton <55567912+Steven-Milton-CS365@users.noreply.github.com>
2019-11-01 20:00:27 -0400

    esoteric changes

--- Pages/Journal/Index.cshtml.cs
+++ Pages/Journal/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -20,12 +21,19 @@ namespace MyScriptureJournal.Pages.Journal
         }
 
         public IList<Models.Journal> Journal { get;set; }
+
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
         // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+
         public SelectList Book { get; set; }
+
         [BindProperty(SupportsGet = true)]
         public string BookSelect { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int SortMeth { get; set;}
+        public List<SelectListItem> SortList { get; set; }
 
         public async Task OnGetAsync()
         {
@@ -34,8 +42,31 @@ namespace MyScriptureJournal.Pages.Journal
                 orderby b.Book
                 select b.Book;
 
+            SortList = new List<SelectListItem>
+            {
+                new SelectListItem{ Value = "1", Text = "None"},
+                new SelectListItem{ Value = "2", Text = "Book"},
+                new SelectListItem{ Value = "3", Text = "Date Added"}
+            };
+
             var notes = from n in _context.Journal
-                select n;
+                select n; ;
+            switch (SortMeth)
+            {
+                case 1:
+                    break;
+                case 2:
+                    notes = from n in _context.Journal
+                        orderby n.Book,n.Reference
+                        select n;
+                    break;
+                case 3:
+                    notes = from n in _context.Journal
+                        orderby n.EntryDate
+                        select n;
+                    break;
+            }
+
             if (!string.IsNullOrEmpty(SearchString))
             {
                 notes = notes.Where(s => s.Notes.Contains(SearchString));
